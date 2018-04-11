13c01549bd80f3d3eaba03a2f6a9785b124c5d73
helloforrestworld <dongweijia238@gmail.com>
2018-04-12 02:29:08 +0800

    歌手数据格式化

---
+++ src/common/js/singFactory.js
@@ -0,0 +1,7 @@
+export class SingFactory {
+  constructor({name, id}) {
+    this.name = name;
+    this.id = id;
+    this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`;
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,6 +6,7 @@
 <script>
 import {getSingerList} from 'api/singer';
 import {ERR_OK} from 'api/config';
+import {SingFactory} from 'common/js/singFactory';
 export default {
   name: 'singer',
   data() {
@@ -20,10 +21,53 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         if (ERR_OK === res.code) {
-          this.singers = res.data.list;
-          console.log(this.singers);
+          this.singers = this._normalize(res.data.list);
         };
       });
+    },
+    _normalize(list) { // 格式化歌手数据
+      let map = {
+        hot: {
+          title: '热门',
+          items: []
+        }
+      };
+      for (let i = 0; i < list.length; i++) {
+        let key = list[i].Findex;
+        if (!(/[a-zA-Z]/.test(key))) {
+          continue;
+        };
+        if (i < 10) {
+          map['hot'].items.push(new SingFactory({
+            name: list[i].Fsinger_name,
+            id: list[i].Fsinger_mid
+          }));
+        };
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        };
+        map[key].items.push(new SingFactory({
+          name: list[i].Fsinger_name,
+          id: list[i].Fsinger_mid
+        }));
+      };
+      // 排序
+      let hot = [];
+      let ret = [];
+      for (let key in map) {
+        if (/[a-zA-Z]/.test(map[key].title)) {
+          ret.push(map[key]);
+        } else {
+          hot.push(map[key]);
+        }
+      };
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      return hot.concat(ret);
     }
   }
 };
