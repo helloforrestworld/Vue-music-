32e3f9fbe138d29271a07e6bca830c63262b46d9
helloforrestworld <dongweijia238@gmail.com>
2018-04-17 11:30:29 +0800

    search-bxo

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box">
+    <i class="icon-dismiss"></i>
+  </div>
+</template>
+<script>
+export default {
+  name: 'search-box',
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped>
+@import "~common/less/variable";
+
+.search-box{
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 6px;
+  height: 40px;
+  background: @color-highlight-background;
+  border-radius: 6px;
+  .icon-search{
+    font-size: 24px;
+    color: @color-background;
+  }
+  .box{
+    flex: 1;
+    margin: 0 5px;
+    line-height: 18px;
+    background: $color-highlight-background;
+    color: @color-text;
+    font-size: @font-size-medium;
+    &::placeholder
+      color: @color-text-d;
+  }
+  .icon-dismiss{
+    font-size: 16px
+    color: @color-background
+  }
+}
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,15 +1,81 @@
 <template>
   <div class="search">
-    我是search
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
   </div>
 </template>
 <script>
+import SearchBox from 'base/search-box/search-box';
 export default {
   name: 'search',
   data() {
     return {};
+  },
+  components: {
+    SearchBox
   }
 };
 </script>
 <style lang="less" scoped>
+@import "~common/less/variable";
+@import "~common/less/mixin";
+
+.search{
+  .search-box-wrapper{
+    margin: 20px;
+  .shortcut-wrapper{
+    position: fixed;
+    top: 178px;
+    bottom: 0;
+    width: 100%;
+    .shortcut{
+      height: 100%;
+      overflow: hidden;
+      .hot-key{
+        margin: 0 20px 20px 20px;
+        .title{
+          margin-bottom: 20px;
+          font-size: @font-size-medium;
+          color: @color-text-l;
+        }
+        .item{
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: @color-highlight-background;
+          font-size: @font-size-medium;
+          color: @color-text-d;
+        }
+      }
+      .search-history{
+        position: relative;
+        margin: 0 20px;
+        .title{
+          display: flex;
+          align-items: center;
+          height: 40px;
+          font-size: @font-size-medium;
+          color: @color-text-l;
+          .text{
+            flex: 1;
+          }
+          .clear{
+            .extend-click();
+            .icon-clear
+              font-size: @font-size-medium;
+              color: @color-text-d;
+          }
+        }
+      }
+    }
+  }
+  .search-result{
+    position: fixed;
+    width: 100%;
+    top: 178px;
+    bottom: 0;
+  }
+}
 </style>
